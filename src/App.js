45d3158b1e5ed7fa44bab85a1b730fd1b273db1b
AvazKayumov <avazkayumov1@gmail.com>
2022-11-21 23:45:06 +0500

    react-hook-form added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
+
 
 function App() {
+  const {
+    register,
+    formState: {errors, isValid},
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onBlur"
+  })
+
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data))
+    reset()
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React Hook Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        <label>
+        Name :
+        <input className="input_text" {...register('firstName', {
+          required: "Name is required!",
+          minLength: {
+            value: 5,
+            message: "First Name must be at least 3 characters long!"
+          },
+          maxLength: {
+            value: 30,
+            message: "First Name must be maximum 30 characters long!"
+          }
+        })}/>
+        </label>
+        <div className="errormessage">
+          {errors?.firstName && <p>{errors?.firstName.message || "Error!"}</p>}
+        </div>
+
+        <label>Email :
+          <input className="input_text" {...register('email', {
+            required: "Email is required!",
+            pattern: {
+              value:  /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              message: "Email must be valid!"
+            }
+          })} />
+        </label>
+        <div className="errormessage">
+          {errors?.email && <p>{errors?.email.message || "Error!"}</p>}
+        </div>
+
+        <label>Password :
+          <input className="input_text" {...register('password', {
+            required: "Password is required!",
+            pattern: {
+              value:  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/,
+              message: "Password must contain at least 6 characters, one Uppercase, one Lowercase, one Number and one special case character!"
+            }
+          })} />
+        </label>
+        <div className="errormessage">
+          {errors?.password && <p>{errors?.password.message || "Error!"}</p>}
+        </div>
+
+
+        <input className="btn_submit" type="submit" value="SUBMIT" />
+      </form>
     </div>
   );
 }
